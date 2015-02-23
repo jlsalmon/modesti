6801e1c13ba55b4beae6788f9f19b8d4610c3099
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-02-23 23:26:34 +0100

    Initial commit

---
+++ src/java/mypackage/CORSFilter.java
@@ -0,0 +1,37 @@
+package mypackage;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CORSFilter implements Filter {
+	
+	public static final Logger logger = LoggerFactory.getLogger(CORSFilter.class);
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		logger.info("filtering");
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		chain.doFilter(req, res);
+	}
+
+	public void init(FilterConfig filterConfig) {}
+
+	public void destroy() {}
+
+}
---
+++ src/java/mypackage/Test.java
@@ -0,0 +1,29 @@
+package mypackage;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by jussy on 21/02/15.
+ */
+@RestController
+public class Test
+{
+	public static final Logger logger = LoggerFactory.getLogger(Test.class);
+	
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    	logger.info("hello!");
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
