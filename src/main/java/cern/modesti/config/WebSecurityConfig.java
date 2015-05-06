81cef681d0217aa9aa8c4e90c69656353981d605
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-05-06 18:41:38 +0200

    #1: Implemented LDAP authentication

--- src/main/java/cern/modesti/config/WebSecurityConfig.java
+++ src/main/java/cern/modesti/config/WebSecurityConfig.java
@@ -3,105 +3,75 @@
  */
 package cern.modesti.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.security.access.event.LoggerListener;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.authentication.BindAuthenticator;
+import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
+import org.springframework.security.ldap.authentication.LdapAuthenticator;
 
 /**
  * @author Justin Lewis Salmon
- *
  */
 @Configuration
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-//  @Override
-//  protected void configure(HttpSecurity http) throws Exception {
-//    http
-//      .authorizeRequests()
-//        .antMatchers("/css/**").permitAll()
-//        .anyRequest().fullyAuthenticated()
-//        .and()
-//      .formLogin();
-//  }
-//
-//  @Configuration
-//  protected static class AuthenticationConfiguration extends
-//      GlobalAuthenticationConfigurerAdapter {
-//
-//    @Override
-//    public void init(AuthenticationManagerBuilder auth) throws Exception {
-//      auth
-//        .ldapAuthentication()
-//          .userDnPatterns("uid={0},ou=people")
-//          .groupSearchBase("ou=groups");
-//          //.contextSource().ldif("classpath:test-server.ldif");
-//    }
-//  }
 
   @Autowired
   Environment env;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-       /*
-        * Set up your spring security config here. For example...
-       */
-//       http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
-      http
-      .authorizeRequests()
-        .antMatchers("/css/**").permitAll()
-        .anyRequest().fullyAuthenticated()
-        .and()
-      .formLogin();
-       /*
-        * Use HTTPs for ALL requests
-       */
-       //http.requiresChannel().anyRequest().requiresSecure();
-       //http.portMapper().http(8080).mapsTo(env.getRequiredProperty("server.port", Integer.class));
+    http.authenticationProvider(ldapAuthenticationProvider());
+
+    http.authorizeRequests().antMatchers("/user/**").permitAll().anyRequest().fullyAuthenticated().and().httpBasic();
   }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
-       authManagerBuilder.authenticationProvider(activeDirectoryLdapAuthenticationProvider()).userDetailsService(userDetailsService());
-  }
+  //    @Configuration
+  //    protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
+  //
+  //      @Override
+  //      public void init(AuthenticationManagerBuilder auth) throws Exception {
+  //        auth
+  //          .ldapAuthentication()
+  //
+  //            .userDnPatterns("CN={0},OU=Users,OU=Organic Units")
+  //            .groupSearchBase("OU=e-groups,OU=Workgroups")
+  //            .contextSource()//.ldif("classpath:test-server.ldif")
+  //            .url("ldaps://cerndc.cern.ch:636/DC=cern,DC=ch");
+  //      }
+  //    }
 
-  @Override
+
   @Bean
-  public AuthenticationManager authenticationManager() {
-       return new ProviderManager(Arrays.asList(activeDirectoryLdapAuthenticationProvider()));
-  }
-  @Bean
-  public AuthenticationProvider activeDirectoryLdapAuthenticationProvider() {
-       ActiveDirectoryLdapAuthenticationProvider provider = new ActiveDirectoryLdapAuthenticationProvider(env.getRequiredProperty("ldap.base"), env.getRequiredProperty("ldap.url"));
-       provider.setConvertSubErrorCodesToExceptions(true);
-       provider.setUseAuthenticationRequestCredentials(true);
-       return provider;
+  public LoggerListener loggerListener() {
+    return new LoggerListener();
   }
 
-//  @Bean
-//  public LdapContextSource contextSource () {
-//      LdapContextSource contextSource= new LdapContextSource();
-//      contextSource.setUrl(env.getRequiredProperty("ldap.url"));
-//      contextSource.setBase(env.getRequiredProperty("ldap.base"));
-////      contextSource.setUserDn(env.getRequiredProperty("ldap.user"));
-////      contextSource.setPassword(env.getRequiredProperty("ldap.password"));
-//      return contextSource;
-//  }
-//
-//  @Bean
-//  public LdapTemplate ldapTemplate() {
-//      return new LdapTemplate(contextSource());
-//  }
+  @Bean
+  public LdapContextSource contextSource() {
+    DefaultSpringSecurityContextSource contextSource = new DefaultSpringSecurityContextSource(env.getRequiredProperty("ldap.url"));
+    contextSource.setBase(env.getRequiredProperty("ldap.base"));
+    return contextSource;
+  }
+
+  @Bean
+  public LdapAuthenticationProvider ldapAuthenticationProvider() {
+    return new LdapAuthenticationProvider(ldapAuthenticator());
+  }
+
+  @Bean
+  public LdapAuthenticator ldapAuthenticator() {
+    BindAuthenticator authenticator = new BindAuthenticator(contextSource());
+    String[] userDnPatterns = new String[]{env.getRequiredProperty("ldap.user")};
+    authenticator.setUserDnPatterns(userDnPatterns);
+    return authenticator;
+  }
 }
--- src/main/java/cern/modesti/filter/CORSFilter.java
+++ src/main/java/cern/modesti/filter/CORSFilter.java
@@ -22,11 +22,12 @@ public class CORSFilter implements Filter {
   @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) res;
-    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Origin", "http://localhost:9000");
     response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3600");
-    response.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type");
+    response.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type, accept, authorization");
     response.setHeader("Access-Control-Expose-Headers", "Location");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
     chain.doFilter(req, res);
   }
 
