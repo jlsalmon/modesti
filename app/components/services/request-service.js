3e3bacea776b90f7202f9e0a745a64743c54e051
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-05-21 14:59:20 +0200

    Refactored request service

--- app/components/services/request-service.js
+++ app/components/services/request-service.js
@@ -5,83 +5,89 @@
  * @name modesti.RequestService
  * @description # RequestService Service in the modesti.
  */
-var app = angular.module('modesti');
+angular.module('modesti').service('RequestService', RequestService);
 
-app.service('RequestService', function($filter, $rootScope, $q, Restangular) {
+function RequestService($filter, $rootScope, $q, Restangular) {
+  var self = this;
 
+  self.cache = {};
+
+  // Public API
   var service = {
+    getRequests: getRequests,
+    getRequest: getRequest,
+    saveRequest: saveRequest,
+    createRequest: createRequest,
+    deleteRequest: deleteRequest,
+    clearCache: clearCache
+  };
 
-    /**
-     *
-     */
-    cachedRequest : null,
+  /**
+   *
+   * @returns {*}
+   */
+  function getRequests() {
+    var q = $q.defer();
 
-    /**
-     *
-     */
-    getRequests : function() {
-      var q = $q.defer();
-
-      Restangular.all('requests').getList().then(function(requests) {
+    Restangular.all('requests').getList().then(function (requests) {
         q.resolve(requests.data);
       },
 
-      function(error) {
+      function (error) {
         console.log('error: ' + error.statusText);
         q.reject(error);
       });
 
-      return q.promise;
-    },
+    return q.promise;
+  }
 
-    /**
-     *
-     */
-    getRequest : function(id, params, unsavedRequest) {
-      var q = $q.defer();
+  /**
+   *
+   * @param id
+   * @param params
+   * @param unsavedRequest
+   * @returns {*}
+   */
+  function getRequest(id, params, unsavedRequest) {
+    var q = $q.defer();
 
-      if (service.cachedRequest && service.cachedRequest.requestId == id) {
-        console.log('using cached request');
+    if (self.cache[id]) {
+      var request = self.cache[id];
+      console.log('using cached request');
 
-        // Merge the given potentially unsaved request with the cached
-        // request. This is because we don't want to lose any unsaved
-        // changes.
-        if (unsavedRequest) {
-          for ( var i in unsavedRequest.points) {
-            // If the point has been modified, update it
-            update(service.cachedRequest.points, unsavedRequest.points[i])
-          }
+      // Merge the given potentially unsaved request with the cached
+      // request. This is because we don't want to lose any unsaved
+      // changes.
+      if (unsavedRequest) {
+        for (var i in unsavedRequest.points) {
+          // If the point has been modified, update it
+          update(request.points, unsavedRequest.points[i])
         }
-
-        // Make a copy for sorting/filtering
-        var request = Restangular.copy(service.cachedRequest);
-
-        if (params) {
-          // Sort/filter the points
-          request.points = transformData(request.points, params.filter(), params);
-        }
-
-        q.resolve(request);
       }
 
-      else {
-        console.log('fetching request ' + id);
+      // Make a copy for sorting/filtering
+      request = Restangular.copy(request);
 
-        Restangular.one('requests', id).get().then(function(request) {
-          var points = request.data.points;
+      if (params) {
+        // Sort/filter the points
+        request.points = transformData(request.points, params.filter(), params);
+      }
 
-          if (points.length == 0) {
-            // Add a starter row
-            points.push({properties: {}});
-          }
+      q.resolve(request);
+    }
+
+    else {
+      console.log('fetching request ' + id);
+
+      Restangular.one('requests', id).get().then(function (response) {
+          var request = response.data;
 
           // Cache the request
-          service.cachedRequest = request.data;
-          //service.cachedRequest.id = id;
-          console.log('cached request (with ' + service.cachedRequest.points.length + ' points)');
+          self.cache[request.requestId] = request;
+          console.log('cached request (with ' + request.points.length + ' points)');
 
           // Make a copy for sorting/filtering
-          request = Restangular.copy(service.cachedRequest);
+          request = Restangular.copy(request);
 
           if (params) {
             // Perform initial sorting/filtering/slicing
@@ -91,119 +97,122 @@ app.service('RequestService', function($filter, $rootScope, $q, Restangular) {
           q.resolve(request);
         },
 
-        function(error) {
+        function (error) {
           console.log(error.status + ' ' + error.statusText);
           q.reject(error);
         });
-      }
+    }
 
-      return q.promise;
-    },
+    return q.promise;
+  }
 
-    /**
-     *
-     */
-    saveRequest : function(request) {
-      $rootScope.saving = "started";
-      var q = $q.defer();
+  /**
+   *
+   * @param request
+   * @returns {*}
+   */
+  function saveRequest(request) {
+    $rootScope.saving = "started";
+    var q = $q.defer();
 
-      // Handle points that have been added, removed, modified or filtered since
-      // the request was last cached.
+    // Handle points that have been added, removed, modified or filtered since
+    // the request was last cached.
 
-      // Merge the given request with the cached request. This is because
-      // the given request may have been filtered, and thus contain less
-      // points. We don't want to accidentally delete points!
-      if (request.points.length < service.cachedRequest.points.length) {
-        for (var i in service.cachedRequest.points) {
-          var point = service.cachedRequest.points[i];
-          // If the cached point isn't in the given request, add it
-          if (!contains(request.points, point)) {
-            request.points.push(point);
-          }
+    // Merge the given request with the cached request. This is because
+    // the given request may have been filtered, and thus contain less
+    // points. We don't want to accidentally delete points!
+    if (request.points.length < self.cache[request.requestId].points.length) {
+      for (var i in self.cache[request.requestId].points) {
+        var point = self.cache[request.requestId].points[i];
+        // If the cached point isn't in the given request, add it
+        if (!contains(request.points, point)) {
+          request.points.push(point);
         }
       }
+    }
 
 
-      // Remove points that have been marked as deleted. Note that points
-      // that have been filtered out will not be deleted.
-      var i = request.points.length;
-      while (i--) {
-        var point = request.points[i];
-        if (point.deleted) {
-          request.points.splice(request.points.indexOf(point), 1);
-        }
+    // Remove points that have been marked as deleted. Note that points
+    // that have been filtered out will not be deleted.
+    var i = request.points.length;
+    while (i--) {
+      var point = request.points[i];
+      if (point.deleted) {
+        request.points.splice(request.points.indexOf(point), 1);
       }
+    }
 
-      request.save().then(function(savedRequest) {
-        console.log('saved request');
+    request.save().then(function (savedRequest) {
+      console.log('saved request');
 
-        // Cache the newly saved request
-        service.cachedRequest = savedRequest.data;
+      // Cache the newly saved request
+      self.cache[request.requestId] = savedRequest.data;
 
-        q.resolve(service.cachedRequest);
-        $rootScope.saving = "success";
+      q.resolve(self.cache[request.requestId]);
+      $rootScope.saving = "success";
 
-      }, function(error) {
-        console.log('error saving request: ' + error.data.message);
-        q.reject(error);
-        $rootScope.saving = "error";
-      });
+    }, function (error) {
+      console.log('error saving request: ' + error.data.message);
+      q.reject(error);
+      $rootScope.saving = "error";
+    });
 
-      return q.promise;
-    },
+    return q.promise;
+  }
 
-    /**
-     *
-     */
-    createRequest : function(request) {
-      var q = $q.defer();
-      var requests = Restangular.all('requests');
+  /**
+   *
+   * @param request
+   * @returns {*}
+   */
+  function createRequest(request) {
+    var q = $q.defer();
+    var requests = Restangular.all('requests');
 
-      requests.post(request).then(function(response) {
+    requests.post(request).then(function (response) {
         var location = response.headers('Location');
         console.log('created request: ' + location);
         q.resolve(location);
       },
 
-      function(error) {
+      function (error) {
         console.log(error.data.message);
-        for ( var i in error.data.errors) {
+        for (var i in error.data.errors) {
           console.log('error: ' + error.data.errors[i].message);
         }
         q.reject(error);
       });
 
-      return q.promise;
-    },
+    return q.promise;
+  }
 
-    /**
-     *
-     */
-    deleteRequest : function(id) {
-      var q = $q.defer();
+  /**
+   *
+   * @param id
+   * @returns {*}
+   */
+  function deleteRequest(id) {
+    var q = $q.defer();
 
-      Restangular.one('requests', id).remove().then(function(response) {
+    Restangular.one('requests', id).remove().then(function (response) {
         console.log('deleted request: ' + response);
         q.resolve(response);
       },
 
-      function(error) {
+      function (error) {
         console.log(error.status + ' ' + error.statusText);
         q.reject(error);
       });
 
-      return q.promise;
-    },
+    return q.promise;
+  }
 
-    /**
-     * 
-     */
-    clearCache : function() {
-      delete service.cachedRequest;
-    }
-  };
-
-  return service;
+  /**
+   *
+   */
+  function clearCache() {
+    self.cache = {};
+  }
 
   /**
    *
@@ -262,4 +271,6 @@ app.service('RequestService', function($filter, $rootScope, $q, Restangular) {
       }
     }
   }
-});
+
+  return service;
+}
