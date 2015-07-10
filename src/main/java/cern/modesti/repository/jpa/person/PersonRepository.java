fddcfab54bf263bec900a940b99db02d94c93824
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-07-10 10:58:59 +0200

    Use person_id rather than cernid

--- src/main/java/cern/modesti/repository/jpa/person/PersonRepository.java
+++ src/main/java/cern/modesti/repository/jpa/person/PersonRepository.java
@@ -30,10 +30,10 @@ import cern.modesti.repository.base.ReadOnlyRepository;
  */
 public interface PersonRepository extends ReadOnlyRepository<Person, String> {
 
-  @Query(value = "SELECT cernid as id, first_name || ' ' || last_name as name "
+  @Query(value = "SELECT person_id as id, first_name || ' ' || last_name as name "
                + "FROM   persons_mv "
                + "WHERE  at_cern = 'Y' AND cern_class = 'STAF' "
-               + "AND   (cernid  LIKE UPPER(:id || '%') "
+               + "AND   (person_id  LIKE UPPER(:id || '%') "
                + "OR     first_name || ' ' || last_name LIKE UPPER('%' || :name || '%'))", nativeQuery = true)
   @Cacheable("persons")
   List<Person> findByIdOrName(@Param("id") String id, @Param("name") String name);
