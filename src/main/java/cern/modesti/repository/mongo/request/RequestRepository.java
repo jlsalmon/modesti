76d66d386e03c9472eefc5a69a0f63632586c50b
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-07-14 15:50:12 +0200

    Added filter query to request repository and applied skinny projection

--- src/main/java/cern/modesti/repository/mongo/request/RequestRepository.java
+++ src/main/java/cern/modesti/repository/mongo/request/RequestRepository.java
@@ -1,49 +1,62 @@
-/*******************************************************************************
- * This file is part of the Technical Infrastructure Monitoring (TIM) project.
- * See http://ts-project-tim.web.cern.ch
- *
- * Copyright (C) 2004 - 2015 CERN. This program is free software; you can
- * redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version. This program is distributed
- * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details. You should have received
- * a copy of the GNU General Public License along with this program; if not,
- * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- *
- * Author: TIM team, tim.support@cern.ch
- ******************************************************************************/
+
 package cern.modesti.repository.mongo.request;
 
+import cern.modesti.request.Request;
+import cern.modesti.request.SkinnyRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import cern.modesti.request.Request;
+import java.util.List;
 
 /**
+ * By setting the excerpt projection on the interface, retrieving a list of requests will in fact return a list of skinny requests. This reduces load greatly.
+ * Retrieving a single request will still return the full request, unless the skinny projection is explicitly requested.
+ *
  * @author Justin Lewis Salmon
  */
-@RepositoryRestResource(collectionResourceRel = "requests", path = "requests")
+@RepositoryRestResource(excerptProjection = SkinnyRequest.class)
 public interface RequestRepository extends MongoRepository<Request, String> {
 
   /**
-   *
    * @param requestId
+   *
    * @return
    */
   Request findOneByRequestId(@Param("requestId") String requestId);
 
   /**
-   *
    * @param criteria
    * @param page
+   *
    * @return
    */
   Page<Request> findAllByOrderByScoreDesc(@Param("q") TextCriteria criteria, Pageable page);
+
+  /**
+   * TODO: this way of querying is probably horribly inefficient. It could probably benefit from indexing of some kind.
+   *
+   * @param status
+   * @param domain
+   * @param type
+   * @param system
+   * @param subsystem
+   * @param creator
+   * @param page
+   *
+   * @return
+   */
+  @Query("{ '$and': [" +
+      "{ 'status': { '$regex': '?0' } }, " +
+      "{ 'domain': { '$regex': '?1' } }, " +
+      "{ 'type':   { '$regex': '?2' } }, " +
+      "{ 'subsystem.system'   : { '$regex': '?3' } }, " +
+      "{ 'subsystem.subsystem': { '$regex': '?4' } }, " +
+      "{ 'creator.username'   : { '$regex': '?5' } } ] }")
+  Page<Request> find(@Param("status") String status, @Param("domain") String domain, @Param("type") String type, @Param("system") String system, @Param
+      ("subsystem") String subsystem, @Param("creator") String creator, Pageable page);
 }
