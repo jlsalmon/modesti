950c1bd7e525b4d58819a7d09f3a39c4af9f82e3
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-08-01 12:22:17 +0200

    #5: Added explicit configuration stage to interface

---
+++ app/components/table/controls/config-controls.directive.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name modesti.directive:configControls
+ * @description # configControls
+ */
+angular.module('modesti').directive('configControls', configControls);
+
+function configControls() {
+  var directive = {
+    templateUrl : 'components/table/controls/config-controls.html',
+    restrict : 'AE',
+    controller : 'ConfigControlsController as ctrl',
+
+    link : function(scope, element, attrs, controller) {
+      controller.init(scope.$parent.ctrl);
+    }
+  };
+
+  return directive;
+}
---
+++ app/components/table/controls/config-controls.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modesti.controller:ConfigControlsController
+ * @description # ConfigControlsController Controller of the modesti
+ */
+angular.module('modesti').controller('ConfigControlsController', ConfigControlsController);
+
+function ConfigControlsController($state, $http, $timeout, RequestService, TaskService) {
+  var self = this;
+
+  self.request = {};
+  self.tasks = {};
+  self.progress = undefined;
+  self.configuring = undefined;
+
+  self.init = init;
+  self.configure = configure;
+
+  /**
+   *
+   */
+  function init(parent) {
+    self.request = parent.request;
+    self.tasks = parent.tasks;
+  }
+
+  /**
+   *
+   */
+  function configure() {
+    var task = self.tasks['configure'];
+    if (!task) {
+      console.log('error configuring request: no task');
+      return;
+    }
+
+    self.configuring = 'started';
+
+    (function getProgress() {
+      console.log('checking progress');
+
+      $http.get(BACKEND_BASE_URL + '/requests/' + self.request.requestId + '/progress').then(function (response) {
+        self.progress = response.data;
+
+        if (self.configuring != 'success' && self.configuring != 'error') {
+          $timeout(getProgress, 100);
+        }
+      });
+    })();
+
+    // Complete the task
+    TaskService.completeTask(task.id, []).then(function (task) {
+      console.log('completed task ' + task.id);
+
+      // Clear the cache so that the state reload also pulls a fresh request
+      RequestService.clearCache();
+
+      $state.reload().then(function() {
+        self.configuring = 'success';
+      });
+    },
+
+    function (error) {
+      console.log('error completing task ' + task.id);
+      self.configuring = 'error';
+    });
+  }
+}
