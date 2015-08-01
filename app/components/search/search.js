44f29ae9d90a67a8d10d6bc826c1c2f9c8f9d3a7
Justin Lewis Salmon <justin2.salmon@live.uwe.ac.uk>
2015-08-01 21:33:49 +0200

    Redesigned search results page

--- app/components/search/search.js
+++ app/components/search/search.js
@@ -7,20 +7,48 @@
  */
 angular.module('modesti').controller('SearchController', SearchController);
 
-function SearchController($location, $stateParams, Restangular) {
+function SearchController($location, $stateParams, $http) {
   var self = this;
   
-  self.results = [];
+  self.requests = [];
+  self.searching = 'started';
   
-  var q = $stateParams.q;
-  console.log('searching for ' + q);
+  self.search = search;
+  self.editRequest = editRequest;
+  
+  search($stateParams.q);
+  
+  /**
+   * 
+   */
+  function search(query) {
+    self.q = query;
+    console.log('searching for ' + self.q);
 
-  // TODO refactor this into a service
-  Restangular.all('requests/search/findAllByOrderByScoreDesc').getList({
-    "q" : q
-  }).then(function(requests) {
-    console.log('got ' + requests.data.length + ' results');
-    self.results = requests.data;
-    self.q = q;
-  });
+    // TODO refactor this into a service
+    $http.get(BACKEND_BASE_URL + '/requests/search/findAllByOrderByScoreDesc', {params: {
+      "q" : self.q
+    }}).then(function(response) {
+      
+      if (response.data.hasOwnProperty('_embedded')) {
+        self.requests = response.data._embedded.requests;
+        console.log('got ' + self.requests.length + ' results');
+      } else {
+        self.requests = [];
+        console.log('no results');
+      }
+
+      self.searching = 'success';
+    });
+  }
+  
+  /**
+  *
+  */
+ function editRequest(request) {
+   var href = request._links.self.href;
+   var id = href.substring(href.lastIndexOf('/') + 1);
+
+   $location.path('/requests/' + id);
+ }
 };
