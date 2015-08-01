6784e59f33350c61986e37ed3556b35d4b078f43
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-08-01 12:06:14 +0200

    Added utility package

---
+++ src/main/java/cern/modesti/util/Util.java
@@ -0,0 +1,38 @@
+package cern.modesti.util;
+
+import cern.modesti.request.point.Point;
+
+import java.util.Map;
+
+/**
+ * TODO
+ *
+ * @author Justin Lewis Salmon
+ */
+public class Util {
+
+  /**
+   *
+   * @param point
+   * @return
+   */
+  public static boolean isEmptyPoint(Point point) {
+    if (point.getProperties().size() == 0) {
+      return true;
+    }
+
+    for (Object subProperty : point.getProperties().values()) {
+      if (subProperty instanceof Map) {
+        for (Object subSubProperty : ((Map) subProperty).values()) {
+          if (subSubProperty != null && !subSubProperty.equals("")) {
+            return false;
+          }
+        }
+      } else if (subProperty != null && !subProperty.equals("")) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
