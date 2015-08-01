25cab39c2a41482da310826b43b76f79a52ff220
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-08-01 12:12:43 +0200

    #5: Added test cases

---
+++ src/test/java/cern/modesti/configuration/ConfigurationServiceTest.java
@@ -0,0 +1,56 @@
+package cern.modesti.configuration;
+
+import cern.c2mon.client.ext.config.ConfigManager;
+import cern.c2mon.shared.client.configuration.ConfigurationReport;
+import cern.modesti.request.Request;
+import cern.modesti.request.point.Point;
+import cern.modesti.security.ldap.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Collections;
+import java.util.HashMap;
+
+/**
+ * TODO
+ *
+ * @author Justin Lewis Salmon
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ConfigurationServiceTest {
+
+  @Mock
+  ConfigManager configManager;
+
+  @InjectMocks
+  ConfigurationService configurationService;
+
+  @Test
+  public void test() {
+    Request request = getTestRequest();
+    ConfigurationReport report = configurationService.configureRequest(request, null);
+  }
+
+  private Request getTestRequest() {
+    Request request = new Request();
+    request.setRequestId("1234");
+    request.setDescription("test request");
+    request.setCreator(new User(1234, "nobody", "No", "Body", "nobody@cern.ch"));
+
+    Point p1 = new Point();
+    p1.setId(1L);
+    p1.setProperties(new HashMap<String, Object>() {{
+      put("pointDescription", "test point 1");
+      put("pointDatatype", "Boolean");
+      put("monitoringEquipment", new HashMap<String, Object>() {{
+        put("id", 500L);
+      }});
+    }});
+
+    request.setPoints(Collections.singletonList(p1));
+    return request;
+  }
+}
