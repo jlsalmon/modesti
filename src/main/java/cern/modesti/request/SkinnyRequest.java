0621d9f862d988e93702dc7d31db65c1c0e4fdeb
Justin Lewis Salmon <justin2.salmon@live.uwe.ac.uk>
2015-08-16 21:23:39 +0200

    Fixed editable states of TIM PLC schema, fixed skinny projection

--- src/main/java/cern/modesti/request/SkinnyRequest.java
+++ src/main/java/cern/modesti/request/SkinnyRequest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Excerpt projection for a {@link Request}. Only the fields returned by the mothods specified in this interface will be returned when this projection is used.
@@ -31,5 +32,5 @@ public interface SkinnyRequest {
 
   SubSystem getSubsystem();
 
-  List<String> getCategories();
+  Set<String> getCategories();
 }
