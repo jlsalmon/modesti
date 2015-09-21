959e00d541c722fc591d94bed8a326d32b669f82
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-09-21 16:54:47 +0200

    Fixed repo method query

--- src/main/java/cern/modesti/request/RequestRepository.java
+++ src/main/java/cern/modesti/request/RequestRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,7 +19,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
  */
 
 @RepositoryRestResource(excerptProjection = RequestProjection.class)
-public interface RequestRepository extends MongoRepository<Request, String> {
+public interface RequestRepository extends MongoRepository<Request, String> /*, QueryDslPredicateExecutor<Request> */{
 
   /**
    * @param requestId
@@ -51,18 +52,15 @@ public interface RequestRepository extends MongoRepository<Request, String> {
    *
    * @return
    */
-  @Query("{ '$and': [                                         " +
-         "    { 'status':            { '$regex': '?0' } },    " +
-         "    { 'domain':            { '$regex': '?1' } },    " +
-         "    { 'type':              { '$regex': '?2' } },    " +
-         "    { 'subsystem':         { '$regex': '?3' } },    " +
-         "    { '$or': [                                      " +
-         "       { 'creator.username':  { '$regex': '?4' } }, " +
-         "       { 'assignee.username': { '$regex': '?5' } }  " +
-         "      ]                                             " +
-         "    }                                               " +
-         "  ]                                                 " +
-         "}                                                   ")
+  @Query("{ '$and': [                                      " +
+         "    { 'status':            { '$regex': '?0' } }, " +
+         "    { 'domain':            { '$regex': '?1' } }, " +
+         "    { 'type':              { '$regex': '?2' } }, " +
+         "    { 'subsystem':         { '$regex': '?3' } }, " +
+         "    { 'creator.username':  { '$regex': '?4' } }, " +
+         "    { 'assignee.username': { '$regex': '?5' } }  " +
+         "  ]                                              " +
+         "}                                                ")
   Page<Request> find(@Param("status") String status, @Param("domain") String domain, @Param("type") String type, @Param("subsystem") String subsystem, @Param
       ("creator") String creator, @Param("assignee") String assignee, Pageable page);
 
