2eef54fbe483a9267c3e4e4df2ad418f229806b0
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2015-09-23 21:23:58 +0200

    Allow "editable" property to take any object

--- src/main/java/cern/modesti/schema/field/Field.java
+++ src/main/java/cern/modesti/schema/field/Field.java
@@ -67,7 +67,7 @@ public class Field {
 
   private Object required;
 
-  private Boolean editable;
+  private Object editable;
 
   private String help_en = "";
 
@@ -75,5 +75,35 @@ public class Field {
 
   public Field(String id) {
     this.id = id;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof Field)) {
+      return false;
+    }
+    Field other = (Field) obj;
+    if (id == null) {
+      if (other.id != null) {
+        return false;
+      }
+    } else if (!id.equals(other.id)) {
+      return false;
+    }
+    return true;
   }
 }
