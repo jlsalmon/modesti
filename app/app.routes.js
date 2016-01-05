6d7bceb9b0a9d5deef00f863e6ce7f87c992797f
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-01-05 16:39:17 +0100

    Switched to HTML5 mode

--- app/app.routes.js
+++ app/app.routes.js
@@ -8,8 +8,10 @@
  */
 angular.module('modesti').config(configureRoutes);
 
-function configureRoutes($stateProvider, $urlRouterProvider) {
+function configureRoutes($stateProvider, $urlRouterProvider, $locationProvider) {
   $urlRouterProvider.otherwise('/');
+
+  $locationProvider.html5Mode(true);
 
   $stateProvider.state('home', {
     url: '/',
