75d70b0fe5a6f9a277540c08b9ee8b8cbb433a40
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-01-14 11:11:42 +0100

    Deleted obsolete files

--- src/main/java/cern/modesti/request/template/TemplateRepository.java
+++
@@ -1,9 +0,0 @@
-package cern.modesti.request.template;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-/**
- * @author Justin Lewis Salmon
- */
-public interface TemplateRepository extends MongoRepository<Template, String> {
-}
