f92054f13c7fed3d0bd1bebae1fdb7bb70fa282c
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-01-20 18:53:25 +0100

    Modified release procedure

--- Gruntfile.js
+++ Gruntfile.js
@@ -567,7 +567,7 @@ module.exports = function (grunt) {
       'build:prod',
       'bump',
       'artifactory:prod:publish',
-      'bump-snapshot'
+      'bump-snapshot:' + version
     ]);
   });
 
