8838c605ed3e438885f459251a061a341368d50e
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-02-15 17:27:03 +0100

    Modified operator comment indication text

--- app/components/request/request.js
+++ app/components/request/request.js
@@ -291,7 +291,7 @@ function RequestController($scope, $q, $state, $timeout, $modal, $filter, $local
     //}
 
     else if (point.properties.approvalResult && point.properties.approvalResult.approved === false) {
-      text = 'Point rejected by operator. Reason: <b>' + point.properties.approvalResult.message + '</b>';
+      text = 'Operator comment: <b>' + point.properties.approvalResult.message + '</b>';
       return '<div class="row-header" data-container="body" data-toggle="popover" data-placement="right" data-html="true" data-content="' +
         text.replace(/"/g, '&quot;') + '">' + point.lineNo + ' <i class="fa fa-comments text-warning"></i></div>';
     }
