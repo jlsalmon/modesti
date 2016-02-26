a24aaa4fbb58213e39c3022ca71603e9aa46f55d
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-02-26 15:28:13 +0100

    Added custom task listener to listen for task assignment events in order to synchronis the request repository

---
+++ src/main/java/cern/modesti/config/WorkflowConfig.java
@@ -0,0 +1,31 @@
+package cern.modesti.config;
+
+import cern.modesti.workflow.task.UserTaskAssignmentHandler;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Justin Lewis Salmon
+ */
+@Configuration
+public class WorkflowConfig {
+
+  @Autowired
+  private UserTaskAssignmentHandler userTaskAssignmentHandler;
+
+  /**
+   * Customise the Activiti {@link org.activiti.engine.ProcessEngineConfiguration}.
+   *
+   * @param engineConfiguration
+   * @return
+   */
+  @Bean
+  public InitializingBean activitiConfigurer(SpringProcessEngineConfiguration engineConfiguration) {
+    return () -> engineConfiguration.getBpmnParser().getBpmnParserHandlers().addHandler(userTaskAssignmentHandler);
+  }
+}
+
+
---
+++ src/main/java/cern/modesti/workflow/task/UserTaskAssignmentHandler.java
@@ -0,0 +1,59 @@
+package cern.modesti.workflow.task;
+
+import cern.modesti.request.Request;
+import cern.modesti.request.RequestRepository;
+import cern.modesti.user.User;
+import cern.modesti.user.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.activiti.bpmn.model.BaseElement;
+import org.activiti.bpmn.model.UserTask;
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+import org.activiti.engine.impl.bpmn.parser.BpmnParse;
+import org.activiti.engine.impl.bpmn.parser.handler.AbstractBpmnParseHandler;
+import org.activiti.engine.impl.bpmn.parser.handler.UserTaskParseHandler;
+import org.activiti.engine.impl.task.TaskDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static java.lang.String.format;
+
+/**
+ * Custom {@link TaskListener} registered globally via custom {@link org.activiti.engine.parse.BpmnParseHandler}
+ * that listens for all "assignment" events on user tasks and synchronises the new assignee to the corresponding
+ * {@link Request} object.
+ *
+ * @author Justin Lewis Salmon
+ */
+@Component
+@Slf4j
+public class UserTaskAssignmentHandler extends AbstractBpmnParseHandler<UserTask> implements TaskListener {
+
+  @Autowired
+  private RequestRepository requestRepository;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public void notify(DelegateTask task) {
+    log.debug(format("handling assignment of task %s", task.getName()));
+
+    // Save the new assignee to the request object
+    Request request = requestRepository.findOneByRequestId((String) task.getVariable("requestId"));
+    User assignee = userRepository.findOneByUsername(task.getAssignee());
+    request.setAssignee(assignee);
+    requestRepository.save(request);
+  }
+
+  @Override
+  protected Class< ? extends BaseElement> getHandledType() {
+    return UserTask.class;
+  }
+
+  @Override
+  protected void executeParse(BpmnParse bpmnParse, UserTask element) {
+    TaskDefinition taskDefinition = (TaskDefinition) bpmnParse.getCurrentActivity().getProperty(UserTaskParseHandler.PROPERTY_TASK_DEFINITION);
+    taskDefinition.addTaskListener(TaskListener.EVENTNAME_ASSIGNMENT, this);
+  }
+}
