3fa5a017c3044152480f152b1620ba25b5730a73
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-03-01 11:02:02 +0100

    Fixed jshint warning

--- app/components/request/controls/addressing-controls.js
+++ app/components/request/controls/addressing-controls.js
@@ -7,7 +7,7 @@
  */
 angular.module('modesti').controller('AddressingController', AddressingController);
 
-function AddressingController($scope, RequestService) {
+function AddressingController($scope) {
   var self = this;
   self.parent = $scope.$parent.ctrl;
 
