91152eceebab721b02226fc5390cfd490b80a392
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-04-28 11:12:41 +0200

    Fix jshint warning

--- app/components/request/request.js
+++ app/components/request/request.js
@@ -582,7 +582,7 @@ function RequestController($scope, $q, $state, $timeout, $modal, $filter, $local
       event.stopPropagation();
     }
 
-    var task = self.tasks.submit || self.tasks.cable || self.tasks.test || self.tasks["remove test flag"];
+    var task = self.tasks.submit || self.tasks.cable || self.tasks.test || self.tasks['remove test flag'];
 
     if (!task) {
       console.log('warning: no submit task found');
