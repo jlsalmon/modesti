3ebe6f27eeb642a2d6212f75d15552a1f136d2c9
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-05-03 18:05:30 +0200

    Allow CORS requests from anywhere

--- src/main/java/cern/modesti/security/filter/CORSFilter.java
+++ src/main/java/cern/modesti/security/filter/CORSFilter.java
@@ -33,7 +33,7 @@ public class CORSFilter implements Filter {
   @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) res;
-    response.setHeader("Access-Control-Allow-Origin", environment.getRequiredProperty("modesti.base"));
+    response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3600");
     response.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type, accept, authorization");
