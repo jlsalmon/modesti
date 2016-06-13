af5694e0b6e6b3945b3d1802b9abf8d9ab6b1376
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-06-13 10:49:08 +0200

    modesti/modesti-csam-plugin#2: Don't redirect when completing CSAM sync

--- app/components/request/controls/for-csam-sync.js
+++ app/components/request/controls/for-csam-sync.js
@@ -2,7 +2,7 @@
 
 angular.module('modesti').controller('CsamSyncController', CsamSyncController);
 
-function CsamSyncController($scope) {
+function CsamSyncController($scope, $state, RequestService, AlertService, TaskService) {
   var self = this;
   self.parent = $scope.$parent.ctrl;
 
@@ -14,6 +14,39 @@ function CsamSyncController($scope) {
       event.stopPropagation();
     }
 
-    self.parent.submit();
+    var task = self.parent.tasks[Object.keys(self.parent.tasks)[0]];
+
+    AlertService.clear();
+    self.submitting = 'started';
+
+    RequestService.saveRequest(self.parent.request).then(function () {
+      console.log('saved request before submitting');
+
+      // Complete the task associated with the request
+      TaskService.completeTask(task.name, self.parent.request.requestId).then(function () {
+        console.log('completed task ' + task.name);
+
+        $state.reload().then(function () {
+
+          if (self.parent.request.status === 'FOR_CSAM_SYNC') {
+            self.submitting = 'error';
+            AlertService.add('danger', 'Synchronisation failed See the error log for details.');
+          } else {
+            self.submitting = 'success';
+            AlertService.add('success', 'Synchronisation completed successfully.');
+          }
+        });
+      },
+
+      function (error) {
+        console.log('error completing task: ' + error.statusText);
+        self.submitting = 'error';
+      });
+    },
+
+    function (error) {
+      console.log('error completing task: ' + error.statusText);
+      self.submitting = 'error';
+    });
   }
 }
