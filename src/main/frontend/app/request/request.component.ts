e28aa09ed9f193b8201431bde032c5a16b6ab93d
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-08-17 15:21:55 +0200

    Fix category switcher error indicators

--- src/main/frontend/app/request/request.component.ts
+++ src/main/frontend/app/request/request.component.ts
@@ -29,35 +29,4 @@ class RequestController {
   public history:any;
   
   public constructor(private $stateParams: any) {}
-
-  /**
-   * Return true if the given category is "invalid", i.e. there are points in
-   * the current request that have errors that relate to the category.
-   *
-   * @param category
-   */
-  public isInvalidCategory(category) {
-    var fieldIds = category.fields.map((field) => field.id);
-    var invalid = false;
-
-    this.request.points.forEach((point) => {
-      if (point.errors && point.errors.length > 0) {
-        point.errors.forEach((error) => {
-          if (!error.category) {
-            var property = error.property.split('.')[0];
-
-            if (fieldIds.indexOf(property) !== -1) {
-              invalid = true;
-            }
-          }
-
-          else if (error.category === category.name || error.category === category.id) {
-            invalid = true;
-          }
-        });
-      }
-    });
-
-    return invalid;
-  }
 }
