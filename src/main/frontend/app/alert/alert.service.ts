58c5118b27714474e32b69bb51eb79312c95bf26
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-08-17 14:22:05 +0200

    Adding type information

--- src/main/frontend/app/alert/alert.service.ts
+++ src/main/frontend/app/alert/alert.service.ts
@@ -1,29 +1,29 @@
 import {Alert} from './alert.ts';
 
 export class AlertService {
-  public static $inject:string[] = ['$rootScope', '$timeout'];
+  public static $inject: string[] = ['$rootScope', '$timeout'];
 
-  public constructor(private $rootScope:any, private $timeout:any) {
+  public constructor(private $rootScope: any, private $timeout: any) {
     // Create an array of globally available alerts
     $rootScope.alerts = [];
   }
 
-  public add(type:string, message:string, timeout:number = 10000) {
-    var alert:Alert = new Alert(type, message, this);
+  public add(type: string, message: string, timeout: number = 10000): void {
+    let alert: Alert = new Alert(type, message, this);
 
     this.$rootScope.alerts.push(alert);
     this.$timeout(() => alert.close(), timeout);
   }
 
-  public closeAlert(alert:Alert) {
+  public closeAlert(alert: Alert): Alert {
     return this.closeAlertByIndex(this.$rootScope.alerts.indexOf(alert));
   }
 
-  public closeAlertByIndex(index:number) {
+  public closeAlertByIndex(index: number): Alert {
     return this.$rootScope.alerts.splice(index, 1);
   }
 
-  public clear() {
-    this.$rootScope.alerts.forEach((alert:Alert) => alert.close());
+  public clear(): void {
+    this.$rootScope.alerts.forEach((alert: Alert) => alert.close());
   }
 }
--- src/main/frontend/app/alert/alert.ts
+++ src/main/frontend/app/alert/alert.ts
@@ -1,10 +1,10 @@
-import {AlertService} from "./alert.service";
+import {AlertService} from './alert.service';
 
 export class Alert {
 
-  constructor(private type:string, private message:string, private alertService:AlertService) {}
+  constructor(private type: string, private message: string, private alertService: AlertService) {}
 
-  public close() {
+  public close(): Alert {
     return this.alertService.closeAlert(this);
   }
 }
