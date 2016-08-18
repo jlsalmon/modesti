2969ce1ad44a844f83e045f843fe0c1781d4591a
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-08-18 08:45:08 +0200

    Add type information

--- src/main/frontend/app/request/request.component.ts
+++ src/main/frontend/app/request/request.component.ts
@@ -3,30 +3,30 @@ import {SchemaService} from '../schema/schema.service';
 import {TaskService} from '../task/task.service';
 
 export class RequestComponent implements ng.IComponentOptions {
-  public templateUrl:string = '/request/request.component.html';
-  public controller:Function = RequestController;
-  public bindings:any = {
+  public templateUrl: string = '/request/request.component.html';
+  public controller: Function = RequestController;
+  public bindings: any = {
     request: '=',
     children: '=',
     schema: '=',
     tasks: '=',
     signals: '=',
-    history: '=',
+    history: '='
   };
 }
 
 class RequestController {
-  public static $inject:string[] = ['$stateParams'];
+  public static $inject: string[] = ['$stateParams'];
 
   /** The handsontable instance */
-  public table:any = {};
+  public table: any = {};
 
-  public request:any;
-  public children:any;
-  public schema:any;
-  public tasks:any;
-  public signals:any;
-  public history:any;
-  
+  public request:  any;
+  public children: any;
+  public schema: any;
+  public tasks: any;
+  public signals: any;
+  public history: any;
+
   public constructor(private $stateParams: any) {}
 }
--- src/main/frontend/app/user/user.component.ts
+++ src/main/frontend/app/user/user.component.ts
@@ -2,18 +2,18 @@ import {AuthService} from '../auth/auth.service';
 import {User} from './user';
 
 export class UserComponent implements ng.IComponentOptions {
-  public templateUrl:string = '/user/user.component.html';
-  public controller:Function = UserController;
+  public templateUrl: string = '/user/user.component.html';
+  public controller: Function = UserController;
 }
 
 class UserController {
-  public static $inject:string[] = ['$stateParams', 'AuthService'];
+  public static $inject: string[] = ['$stateParams', '$rootScope', 'AuthService'];
 
-  public user:User;
+  public user: User;
 
-  constructor(private $stateParams:any, private authService:AuthService) {}
+  constructor(private $stateParams: any, private $rootScope: any, private authService: AuthService) {}
 
-  public $onInit() {
-    this.authService.getUser(this.$stateParams.id).then((user) => this.user = user);
+  public $onInit(): void {
+    this.authService.getUser(this.$stateParams.id).then((user: User) => this.user = user);
   }
 }
