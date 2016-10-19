fe66f33c34dc862fdbd0e3198ea702c2cd91f7b3
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-10-19 11:58:31 +0200

    Add dependency to latinize.js

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -5,6 +5,7 @@
 /// <reference path="globals/es6-promise/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/latinize/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/moment-node/index.d.ts" />
 /// <reference path="globals/moment/index.d.ts" />
