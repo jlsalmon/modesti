27d72359ab6ad6c176692893bd9dcb8c4ba04c67
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-10-26 15:49:15 +0200

    Remove unused imports

--- src/main/java/cern/modesti/Application.java
+++ src/main/java/cern/modesti/Application.java
@@ -1,17 +1,11 @@
 package cern.modesti;
 
 import cern.modesti.plugin.RequestProvider;
-import org.activiti.engine.RepositoryService;
-import org.activiti.engine.TaskService;
 import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.plugin.core.config.EnablePluginRegistries;
-
-import java.net.URL;
-import java.net.URLClassLoader;
 
 /**
  * The main entry point for the MODESTI application server.
