7545659d3af15cbac1e9d080bc0b985381eef8c2
Martin Flamm <martin.flamm@cern.ch>
2016-10-26 11:27:35 +0200

    #138: Added possibility to inline edit the request description
    As meantioned on Issue #138, you can now edit the request description in the request detail view by clicking on it. Empty descriptions are not allowed.

--- src/main/frontend/app/request/header/header.component.ts
+++ src/main/frontend/app/request/header/header.component.ts
@@ -1,4 +1,6 @@
 import IComponentOptions = angular.IComponentOptions;
+import {RequestService} from '../request.service';
+import {Request} from '../request';
 
 export class RequestHeaderComponent implements IComponentOptions {
   public templateUrl: string = '/request/header/header.component.html';
@@ -8,4 +10,23 @@ export class RequestHeaderComponent implements IComponentOptions {
   };
 }
 
-class RequestHeaderController {}
+class RequestHeaderController {
+  public static $inject: string[] = ['RequestService'];
+
+  public request: Request;
+  public hover: boolean = false;
+
+  public constructor(private requestService: RequestService) {}
+
+  public validateDescription(data): String {
+    if (!(data.toString().length > 0)) {
+      return ' ';
+    }
+  }
+
+  public saveRequest(data): void {
+    this.requestService.saveRequest(this.request).then((request: Request) => {
+      this.request = request;
+    });
+  }
+}
