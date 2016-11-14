d88a75570d622c981758e512bc08bba38f00043e
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-11-14 11:05:29 +0100

    Remove obsolete methods from RequestProvider

--- src/main/java/cern/modesti/plugin/RequestProvider.java
+++ src/main/java/cern/modesti/plugin/RequestProvider.java
@@ -2,14 +2,7 @@ package cern.modesti.plugin;
 
 import cern.modesti.plugin.metadata.MetadataProvider;
 import cern.modesti.request.Request;
-import cern.modesti.request.point.Point;
-import cern.modesti.request.upload.parser.RequestParser;
-import cern.modesti.workflow.validation.RequestValidator;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.plugin.core.Plugin;
-
-import static java.lang.String.format;
 
 /**
  * An implementation must extend this class to be registered as a plugin.
@@ -27,20 +20,5 @@ public abstract class RequestProvider implements Plugin<Request>, MetadataProvid
   @Override
   public boolean supports(Request request) {
     return getMetadata().getId().equals(request.getDomain());
-  }
-
-  /**
-   * Plugins that wish to provide upload support from Excel sheets must
-   * override this method and return a {@link RequestParser} implementation.
-   *
-   * @return a {@link RequestParser} capable of parsing a {@link Request}
-   * instance of this domain from an Excel sheet.
-   */
-  public RequestParser getRequestParser() {
-    throw new UnsupportedRequestException(format("Plugin for domain %s does not provide a RequestParser implementation", getMetadata().getId()));
-  }
-
-  public RequestValidator getRequestValidator() {
-    return null;
   }
 }
