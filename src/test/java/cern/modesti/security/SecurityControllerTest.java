306767c2f1e85b764272508b0f50f4423074d20b
Martin Flamm <martin.flamm@cern.ch>
2016-12-16 11:16:26 +0100

    #148: Upgraded to Spring 4.3.4.RELEASE and Spring Boot 1.4.2.RELEASE

--- src/test/java/cern/modesti/security/SecurityControllerTest.java
+++ src/test/java/cern/modesti/security/SecurityControllerTest.java
@@ -14,14 +14,14 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
@@ -32,8 +32,8 @@ import cern.modesti.Application;
  * @author Justin Lewis Salmon
  *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {Application.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 @TestPropertySource("classpath:modesti-test.properties")
 @WebAppConfiguration
 public class SecurityControllerTest {
