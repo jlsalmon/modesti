57e8b258e219117ce13050d3540b2e97e14958d7
iprietob <iprietob@cern.ch>
2017-06-23 18:02:49 +0200

    #211: Add the 'id' property to the request points

--- modesti-server/src/test/java/cern/modesti/TestUtil.java
+++ modesti-server/src/test/java/cern/modesti/TestUtil.java
@@ -1,15 +1,16 @@
 package cern.modesti;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+
+import cern.modesti.point.Point;
 import cern.modesti.point.PointImpl;
 import cern.modesti.request.Request;
 import cern.modesti.request.RequestImpl;
 import cern.modesti.request.RequestType;
-import cern.modesti.point.Point;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Justin Lewis Salmon
@@ -29,8 +30,10 @@ public class TestUtil {
     ArrayList<Point> points = new ArrayList<>();
     Point point1 = new PointImpl();
     Point point2 = new PointImpl();
-    point1.setProperties(Maps.newHashMap(ImmutableMap.of("pointDescription", "TEST POINT 1", "pointDatatype", "Boolean")));
-    point2.setProperties(Maps.newHashMap(ImmutableMap.of("pointDescription", "TEST POINT 2", "pointDatatype", "Boolean")));
+    point1.setProperties(
+        Maps.newHashMap(ImmutableMap.of("id", "1", "pointDescription", "TEST POINT 1", "pointDatatype", "Boolean")));
+    point2.setProperties(
+        Maps.newHashMap(ImmutableMap.of("id", "2", "pointDescription", "TEST POINT 2", "pointDatatype", "Boolean")));
     points.add(point1);
     points.add(point2);
     return points;
