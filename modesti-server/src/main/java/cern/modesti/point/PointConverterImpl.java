93d6bdfe6728153361f2a591519f52fde30d9cd7
Matthias Braeger <Matthias.Braeger@cern.ch>
2017-08-23 12:19:14 +0200

    Refactor PointConverterImpl to prepare code share with tim-configurator

--- modesti-server/src/main/java/cern/modesti/point/PointConverterImpl.java
+++ modesti-server/src/main/java/cern/modesti/point/PointConverterImpl.java
@@ -1,22 +1,24 @@
 package cern.modesti.point;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Component;
-
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
 
 import static java.lang.String.format;
 
 /**
+ * TODO: Move to modesti-api to share with tim-configurator
  * @author Justin Lewis Salmon
  */
 @Slf4j
@@ -26,8 +28,27 @@ public class PointConverterImpl implements PointConverter {
   @Override
   public <T> Page<Point> convert(Page<T> pointsToConvert, Class<T> klass, Pageable pageable) {
     List<Point> points = new ArrayList<>();
-    BeanInfo beanInfo;
 
+    for (T pointToConvert : pointsToConvert) {
+      points.add(convert(pointToConvert, klass));
+    }
+
+    return new PageImpl<>(points, pageable, pointsToConvert.getTotalElements());
+  }
+
+  /**
+   * Used to convert either TimPoint, CsamPoint or WinCCOAPoint
+   */
+  public static <T> List<Point> convert(List<T> pointsToConvert, Class<T> klass) {
+    List<Point> points = Collections.synchronizedList(new ArrayList<>());
+    pointsToConvert.stream().parallel().forEach(t -> points.add(convert(t, klass)));
+    return points;
+  }
+
+  public static <T> Point convert(T pointToConvert, Class<T> klass) {
+    Point point = new PointImpl();
+
+    BeanInfo beanInfo;
     try {
       beanInfo = Introspector.getBeanInfo(klass);
     } catch (IntrospectionException e) {
@@ -35,40 +56,33 @@ public class PointConverterImpl implements PointConverter {
       throw new RuntimeException(e);
     }
 
-    for (T t : pointsToConvert) {
-      Point point = new PointImpl();
-
-      for (PropertyDescriptor propertyDescriptor : beanInfo.getPropertyDescriptors()) {
-        String propertyName = propertyDescriptor.getName();
-        if (propertyName.equals("class")) {
-          continue;
-        }
-
-        Object value;
-
-        try {
-          value = propertyDescriptor.getReadMethod().invoke(t);
-          Class<?> type = propertyDescriptor.getPropertyType();
-
-          // This is a hack to make sure that all Object properties are constructed with null fields inside
-          // because handsontable craps out otherwise
-          if (value == null && !type.isPrimitive() && !type.equals(String.class) && !type.equals(Integer.class)
-              && !type.equals(Long.class) && !type.equals(Class.class) && !type.equals(Float.class)) {
-
-            value = propertyDescriptor.getPropertyType().newInstance();
-          }
-
-        } catch (InvocationTargetException | IllegalAccessException | InstantiationException e) {
-          log.error(format("Error converting point of type %s", klass), e);
-          throw new RuntimeException(e);
-        }
-
-        point.addProperty(propertyName, value);
+    for (PropertyDescriptor propertyDescriptor : beanInfo.getPropertyDescriptors()) {
+      String propertyName = propertyDescriptor.getName();
+      if (propertyName.equals("class")) {
+        continue;
       }
 
-      points.add(point);
-    }
+      Object value;
 
-    return new PageImpl<>(points, pageable, pointsToConvert.getTotalElements());
+      try {
+        value = propertyDescriptor.getReadMethod().invoke(pointToConvert);
+        Class<?> type = propertyDescriptor.getPropertyType();
+
+        // This is a hack to make sure that all Object properties are constructed with null fields inside
+        // because handsontable craps out otherwise
+        if (value == null && !type.isPrimitive() && !type.equals(String.class) && !type.equals(Integer.class)
+            && !type.equals(Long.class) && !type.equals(Class.class)  && !type.equals(Float.class)) {
+
+          value = propertyDescriptor.getPropertyType().newInstance();
+        }
+
+      } catch (InvocationTargetException | IllegalAccessException | InstantiationException e) {
+        log.error(format("Error converting property %s of point type %s", propertyName, klass), e);
+        throw new RuntimeException(e);
+      }
+
+      point.addProperty(propertyName, value);
+    }
+    return point;
   }
 }
