7a45226eb14bfa7853fcb5960e9f75bb3c693986
iprietob <iprietob@cern.ch>
2017-08-31 14:25:05 +0200

    #217: Add execute method
    The execute method will allow to apply specific actions on a Request

--- modesti-api/src/main/java/cern/modesti/request/RequestService.java
+++ modesti-api/src/main/java/cern/modesti/request/RequestService.java
@@ -1,5 +1,8 @@
 package cern.modesti.request;
 
+import cern.modesti.user.User;
+import cern.modesti.workflow.request.RequestAction;
+
 /**
  * Service class for creating, updating, deleting and searching for
  * {@link Request} objects.
@@ -15,4 +18,14 @@ public interface RequestService {
   void delete(Request request);
 
   Request findOneByRequestId(String requestId);
+  
+  /**
+   * Execute an action on a {@link Request} inside a workflow process instance.
+   *
+   * @param requestId the id of the request
+   * @param action    the action to execute
+   * @param user      the user who is performing the action
+   * @return the updated request 
+   */
+  Request execute(String requestId, RequestAction action, User user);
 }
