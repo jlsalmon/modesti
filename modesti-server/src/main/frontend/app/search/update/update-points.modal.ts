f491e3b28b7c23a2bfc5ae5d0ba9416d4c6d1e94
Eleni Mandilara <eleni.mandilara@cern.ch>
2017-10-20 17:07:21 +0200

    Add submit function for creating new request

--- modesti-server/src/main/frontend/app/search/update/update-points.modal.ts
+++ modesti-server/src/main/frontend/app/search/update/update-points.modal.ts
@@ -5,35 +5,55 @@ import {Point} from '../../request/point/point';
 import {Schema} from '../../schema/schema';
 import {Field} from '../../schema/field/field';
 import IPromise = angular.IPromise;
+import IFormController = angular.IFormController;
+import {RequestService} from "../../request/request.service";
 
 export class UpdatePointsModalController {
-  public static $inject: string[] = ['$uibModalInstance', 'points', 'schema', 'AuthService', 'SchemaService'];
+  public static $inject: string[] = ['$uibModalInstance', 'points', 'schema', 'AuthService', 'SchemaService', '$state', 'RequestService'];
 
   public request: Request;
   public fieldValues: any[];
+  public submitting: string = undefined;
 
   constructor(private $modalInstance: any, private points: Point[], private schema: Schema,
-              private authService: AuthService, private schemaService: SchemaService) {
-    this.request = new Request();
-    this.request.type = 'UPDATE';
-    this.request.description = '';
-    this.request.creator = authService.getCurrentUser().username;
-    this.request.assignee = this.request.creator;
-    this.request.domain = schema.id;
-    this.request.points = points;
+              private authService: AuthService, private schemaService: SchemaService, private $state: any,private requestService: RequestService) {
+        this.request = new Request();
+        this.request.type = 'CREATE';
+        this.request.contractName = '';
+        this.request.publisher = '';
+        this.request.subscriber = '';
+        this.request.creator = authService.getCurrentUser().username;
+        this.request.assignee = this.request.creator;
+        this.request.domain = schema.id;
+        this.request.points = [];
+        this.request.description = '';
+        this.request.parentRequestId = null;
+        this.request.status = 'IN_PROGRESS';
   }
 
-  public ok(): void {
-    this.$modalInstance.close(this.request);
-  }
+    public ok(): void {
+        this.$modalInstance.close(this.request);
+    }
+
+    public submit(form: IFormController): void {
+        this.request.description = this.request.contractName;
+        this.submitting = 'started';
+        this.request.assignee = this.request.creator;
+        // Post form to server to create new request.
+        this.requestService.createRequest(this.request).then((location: string) => {
+              // Strip request ID from location.
+              let id: string = location.substring(location.lastIndexOf('/') + 1);
+            });
+        this.$modalInstance.dismiss('cancel');
+    }
 
   public cancel(): void {
-    this.$modalInstance.dismiss('cancel');
+        this.$modalInstance.dismiss('cancel');
   }
 
   public queryFieldValues(field: Field, query: string): IPromise<any> {
-    return this.schemaService.queryFieldValues(field, query, undefined).then((values: any[]) => {
-      this.fieldValues = values;
-    });
-  }
+        return this.schemaService.queryFieldValues(field, query, undefined).then((values: any[]) => {
+          this.fieldValues = values;
+        });
+      }
 }
