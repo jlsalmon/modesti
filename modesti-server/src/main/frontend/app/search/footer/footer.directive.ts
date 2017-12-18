517d1fd00e434de5e61417da4e4ad261513b26e2
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2017-12-18 10:45:08 +0100

    #233: Add footer directive for the search controls

--- modesti-server/src/main/frontend/app/search/footer/footer.directive.ts
+++ modesti-server/src/main/frontend/app/search/footer/footer.directive.ts
@@ -19,10 +19,11 @@ export class SearchFooterDirective implements IDirective {
     table: '='
   }
 
-  public constructor(private $compile: ICompileService, private $http: IHttpService, private $ocLazyLoad: any, private schemaService: SchemaService, private tableService: TableService) { }
+  public constructor(private $compile: ICompileService, private $http: IHttpService, private $ocLazyLoad: any, 
+	private schemaService: SchemaService, private tableService: TableService) { }
 
   public static factory(): IDirectiveFactory {
-    const directive: IDirectiveFactory = ($compile: ICompileService, $http: IHttpService, $ocLazyLoad: any, schemaService: SchemaService, tableService : TableService) =>
+    const directive: IDirectiveFactory = ($compile: ICompileService, $http: IHttpService, $ocLazyLoad: any, schemaService: SchemaService, tableService : TableService, defaultSearchControls: DefaultSearchControls) =>
       new SearchFooterDirective($compile, $http, $ocLazyLoad, schemaService, tableService);
     directive.$inject = ['$compile', '$http', '$ocLazyLoad', 'SchemaService', 'TableService'];
     return directive;
@@ -40,64 +41,70 @@ export class SearchFooterDirective implements IDirective {
 
         this.$http.get('/api/plugins/' + schemaId + '/search-assets').then((response: any) => {
           let assets: string[] = response.data;
-          console.log(assets);
 
-          let directiveName : string;
-          assets.forEach((asset: string) => {
-            if (asset.endsWith("-search-controls.js")) {
-              directiveName = asset.split('/')[asset.split('/').length - 1];
-              directiveName = directiveName.substring(0, directiveName.length - 3);
-            }
-          }
-
-          this.$ocLazyLoad.load(assets, {serie: true}).then(() => {
-            let controlsName = directiveName;
-            let template: string = '<div ' + controlsName + ' table="$ctrl.table" ng-show="$ctrl.isVisible(\'' + schemaId + '\')"></div>';
-            element.append(this.$compile(template)(scope));
-          };
-
+          if (assets.length == 0) {
+            this.loadDefaultSearchButtons(schemaId, element, scope);
+          } else {
+            this.loadPluginSearchButtons(schemaId, assets, element, scope);
+         }
         });
       });
     });
   }
+
+
+  public loadDefaultSearchButtons(schemaId: string, element: any, scope: any) : void {
+    let template: string = 
+      '<div class="search-controls container-fluid" table="$ctrl.table" ng-show="$ctrl.isVisible(\'' + schemaId + '\')"><search-controls></search-controls></div>';
+    element.append(this.$compile(template)(scope));
+  }
+
+
+  public loadPluginSearchButtons(schemaId: string, assets : string[], element: any, scope: any) : void {
+    let directiveName : string;
+    assets.forEach((asset: string) => {
+      if (asset.endsWith("-search-controls.js")) {
+        directiveName = asset.split('/')[asset.split('/').length - 1];
+        directiveName = directiveName.substring(0, directiveName.length - 3);
+      }
+    }
+
+    this.$ocLazyLoad.load(assets, {serie: true}).then(() => {
+      let controlsName = directiveName;
+      let template: string = '<div ' + controlsName + ' table="$ctrl.table" ng-show="$ctrl.isVisible(\'' + schemaId + '\')"></div>';
+      element.append(this.$compile(template)(scope));
+    };
+  }
 }
 
-
 class SearchFooterController {
-  public static $inject: string[] = ['$rootScope', '$uibModal', '$scope', '$state', '$q', 'SearchService', 'RequestService', 'AlertService', 'TableService'];
+  public static $inject: string[] = ['$rootScope', 'TableService'];
   
   public activeSchemaId : string = "";
   public schema: Schema;
   public schemas: Schema[];
   public page: any = {number: 0, size: 100};
-
-  public constructor(private $rootScope: IRootScope, private $modal: any, private $scope: IScope, private $state: IStateService, private $q: IQService,
-                     private searchService: SearchService, private requestService: RequestService, private alertService: AlertService, 
-                     private tableService: TableService) {
+ 
+  public constructor(private $rootScope: IRootScope, private tableService: TableService) {
     $rootScope.$on('modesti:searchDomainChanged', (event, data) => {
       this.activeSchemaId = data;
     });
   }
 
   public isVisible(schemaId: string) : boolean {
-    console.log("Cheking if the schema is visible: " + schemaId);
     return this.activeSchemaId !== undefined && this.activeSchemaId == schemaId;
   }
 
-  public parentTest(): void {
-    console.log("This is a test from the parent controller");
-  } 
-
-  public updatePoints(): void {
-    this.tableService.updatePoints();
+  public updatePoints(message: string=''): void {
+    this.tableService.updatePoints(message);
   }
 
   public deletePoints(): void {
-    this.$scope.$parent.$ctrl.deletePoints();
+    this.tableService.deletePoints();
   }
 
   public getTable(): Table {
-    return this.$scope.$parent.$ctrl.table;
+    return this.tableService.table;
   }
 }
 
