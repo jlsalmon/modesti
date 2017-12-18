58c7006360d260b1864b26260fff5c5865d34a7b
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2017-12-18 10:50:48 +0100

    #233: Add default search controls

---
+++ modesti-server/src/main/frontend/app/search/footer/search.controls.component.ts
@@ -0,0 +1,31 @@
+import IComponentOptions = angular.IComponentOptions;
+import {TableService} from '../table.service';
+
+
+export class SearchControlsComponent implements IComponentOptions {
+  public templateUrl: string = '/search/footer/search.controls.component.html';
+  public controller: Function = SearchControlsController;
+  public bindings: any = {
+    request: '=' 
+  };
+}
+
+class SearchControlsController {
+  public static $inject: string[] = ['TableService'];
+
+  public constructor(private tableService: TableService) {}
+
+  public updatePoints(): void {
+    this.tableService.updatePoints();
+  }
+
+  public deletePoints(): void {
+    this.tableService.deletePoints();
+  }
+
+  public getTable(): Table {
+    return this.tableService;
+  }
+}
+
+
