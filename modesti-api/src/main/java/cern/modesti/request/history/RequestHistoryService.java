927f8e4f7b5434efd43dcac81fe8518146589649
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2018-02-14 17:17:26 +0100

    #241: New methods for the request history service

--- modesti-api/src/main/java/cern/modesti/request/history/RequestHistoryService.java
+++ modesti-api/src/main/java/cern/modesti/request/history/RequestHistoryService.java
@@ -9,5 +9,36 @@ import java.util.List;
  */
 public interface RequestHistoryService {
 
+  /**
+   * Get the list of changes for the given request.
+   * @param request the request object
+   * @return
+   */
   List<Change> getChanges(Request request);
+  
+  /**
+   * Create a new entry in the history repository for the given request.
+   *
+   * @param request the request object
+   */
+  void initialiseChangeHistory(Request request);
+  
+  /**
+   * Compare the changes from the given, modified request to the original
+   * request and save them to the history record.
+   *
+   * Currently we are only storing a single change event, diffed from the
+   * original request. This only makes sense for UPDATE requests. Storing
+   * successive diffs for CREATE requests would probably be too complicated.
+   *
+   * @param modified the modified request
+   */
+  void saveChangeHistory(Request modified);
+  
+  
+  /**
+   * Delete the change history of a request.
+   * @param request the request object
+   */
+  void deleteChangeHistory(Request request);
 }
