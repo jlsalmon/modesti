16f12fc20c7181ac5e9c6bfe705233ee8ab8b889
Eleni Mandilara <eleni.mandilara@cern.ch>
2018-02-19 13:14:44 +0100

    #243: Fix bug causing the never dispatching of AJAX request to fetch data for autocomplete field type

--- modesti-server/src/main/frontend/app/search/update/update-points.modal.ts
+++ modesti-server/src/main/frontend/app/search/update/update-points.modal.ts
@@ -4,14 +4,14 @@ import {Request} from '../../request/request';
 import {Point} from '../../request/point/point';
 import {Schema} from '../../schema/schema';
 import {Field} from '../../schema/field/field';
-//import IPromise = angular.IPromise;
+import IPromise = angular.IPromise;
 import {IPromise, IRootScopeService} from 'angular';
 
 export class UpdatePointsModalController {
   public static $inject: string[] = ['$uibModalInstance', '$rootScope', 'points', 'schema', 'message', 'AuthService', 'SchemaService'];
 
   public request: Request;
-  public fieldValues: any[];
+  public fieldValues: any[] = [];
 
   constructor(private $modalInstance: any, private $rootScope: IRootScopeService, private points: Point[], private schema: Schema, private message: string,
               private authService: AuthService, private schemaService: SchemaService) {
@@ -33,9 +33,9 @@ export class UpdatePointsModalController {
     this.$modalInstance.dismiss('cancel');
   }
 
-  public queryFieldValues(field: Field, query: string): IPromise<any> {
-    return this.schemaService.queryFieldValues(field, query, undefined).then((values: any[]) => {
-      this.fieldValues = values;
-    });
-  }
+   public queryFieldValues(field: Field, query: string): IPromise<void> {
+      return this.schemaService.queryFieldValues(field, query, undefined).then((values: any[]) => {
+        this.fieldValues = values;
+      });
+    }
 }
