c9ebc1c69faaa7bd3206e671b8b137a296de5b06
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2018-05-08 15:58:35 +0200

    [MOD-212] Add field 'showFieldsOnClone'

--- modesti-server/src/main/frontend/app/schema/configuration/configuration.ts
+++ modesti-server/src/main/frontend/app/schema/configuration/configuration.ts
@@ -1,9 +1,11 @@
 export class Configuration {
     public createFromUi: boolean;
+    public showFieldsOnClone: boolean;
 
     public deserialize(config: Configuration): Configuration {
         this.createFromUi = config.createFromUi;
-
+        this.showFieldsOnClone = config.showFieldsOnClone;
+        
         return this;
     }
 
