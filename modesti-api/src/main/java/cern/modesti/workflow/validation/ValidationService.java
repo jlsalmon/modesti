f2027215160e06abf8fd51951514133c7d404804
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2019-05-17 14:47:24 +0200

    New method to pre-validate a request

--- modesti-api/src/main/java/cern/modesti/workflow/validation/ValidationService.java
+++ modesti-api/src/main/java/cern/modesti/workflow/validation/ValidationService.java
@@ -10,8 +10,19 @@ public interface ValidationService {
   /**
    * Validate a request
    * 
-   * @param request
-   * @return
+   * @param request The request to validate
+   * @return TRUE if and only if the request is valid
    */
   boolean validateRequest(Request request);
+  
+  /**
+   * Pre validates a request. The method will be executed for requests
+   * created from the REST service without using the MODESTI interface to 
+   * verify that the minimum required information is provided (i.e. UPDATE/DELETE requests
+   * contain at least the primary field)
+   * 
+   * @param request The request to pre-validate
+   * @return TRUE if and only if the request is valid
+   */
+  boolean preValidateRequest(Request request);
 }
