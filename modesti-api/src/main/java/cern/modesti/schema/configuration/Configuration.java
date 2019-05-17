5af3cf7b03703f58c9fd98c4c5554cd2589e92e8
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2019-05-17 14:45:39 +0200

    Add javadoc

--- modesti-api/src/main/java/cern/modesti/schema/configuration/Configuration.java
+++ modesti-api/src/main/java/cern/modesti/schema/configuration/Configuration.java
@@ -11,25 +11,25 @@ public interface Configuration extends Serializable {
 
   /**
    * Specifies if the creation of requests from the UI is allowed
-   * @return
+   * @return TRUE if and only if the creation of requests from the UI is allowed
    */ 
   boolean isCreateFromUi();
   
   /**
    * Specifies if the cloning of requests from the UI is allowed
-   * @return
+   * @return TRUE if and only if the cloning of requests from the UI is allowed
    */ 
   boolean isCloneFromUi();
   
   /**
    * Specifies if the clone requests must show the schema fields
-   * @return
+   * @return TRUE if and only if the clone requests must show the schema fields
    */
   boolean isShowFieldsOnClone();
   
   /**
    * Specifies if the delete requests must show the schema fields
-   * @return
+   * @return TRUE if and only if the delete requests must show the schema fields
    */
   boolean isShowFieldsOnDelete();
 }
