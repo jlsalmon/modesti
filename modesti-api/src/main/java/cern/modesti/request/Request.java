e2ccc02f8c0daeddf8779290c4211b083b36fc4d
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2019-05-17 14:42:42 +0200

    Deprecating methods. Adding methods to check if a request has been generated from the UI

--- modesti-api/src/main/java/cern/modesti/request/Request.java
+++ modesti-api/src/main/java/cern/modesti/request/Request.java
@@ -143,6 +143,7 @@ public interface Request extends Serializable {
    * when UPDATE/DELETE requests provide the modified fields/point IDs.
    * @return TRUE if and only if the core validation must be skipped
    */
+  @Deprecated
   boolean isSkipCoreValidation();
   
   /**
@@ -150,7 +151,24 @@ public interface Request extends Serializable {
    * when UPDATE/DELETE requests provide the modified fields/point IDs.
    * @param skip Value for the skipCoreValidation flag
    */
+  @Deprecated
   void setSkipCoreValidation(boolean skip);
+  
+  /**
+   * Checks is the request is generated from the MODESTI UI, opposite than created from a REST request.
+   * Requests generated from the UI must stop in the workflow stage IN_PROGRESS.
+   * On the opposite, requests generated from the REST service already contain the necessary changes and
+   * can continue in the workflow.
+   * 
+   * @return TRUE if and only if the request has been generated from the MODESTI UI
+   */
+  boolean isGeneratedFromUi();
+  
+  /**
+   * Sets the value of the flag 'generatedFromUi'.
+   * @param value Value of the flag 'generatedFromUi'
+   */
+  void setGeneratedFromUi(boolean value);
 
   /**
    * Gets the list of points in the request
