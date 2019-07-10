14380fb4ee3e0c3767189bf32e4be815592ab210
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2019-07-10 16:32:23 +0200

    [MOD-195] Change to AbstractAuthenticationToken

--- modesti-server/src/main/java/cern/modesti/workflow/task/TaskController.java
+++ modesti-server/src/main/java/cern/modesti/workflow/task/TaskController.java
@@ -1,7 +1,6 @@
 package cern.modesti.workflow.task;
 
 import cern.modesti.user.User;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
@@ -9,7 +8,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,7 +31,6 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @Controller
 @RequestMapping("/api/requests/{id}/tasks")
 @PreAuthorize("permitAll")
-@Slf4j
 public class TaskController {
 
   @Autowired
@@ -66,7 +64,7 @@ public class TaskController {
   @RequestMapping(value = "/{name}", method = POST, produces = "application/hal+json")
   public HttpEntity<Resource<TaskInfo>> action(@PathVariable("id") String id, @PathVariable("name") String taskName, @RequestBody TaskAction action,
                                                Principal principal) {
-    User user = (User) ((UsernamePasswordAuthenticationToken) principal).getPrincipal();
+    User user = (User) ((AbstractAuthenticationToken) principal).getPrincipal();
     TaskInfo task = taskService.execute(id, taskName, action, user);
 
     if (task != null) {
