bc0b2bef01e2aefda0f1f470c26f0e6c6ec58f9d
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2019-07-22 16:51:48 +0200

    Add javadoc comments

--- modesti-server/src/main/java/cern/modesti/request/RequestProjection.java
+++ modesti-server/src/main/java/cern/modesti/request/RequestProjection.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
 
 /**
- * Excerpt projection for a {@link Request}. Only the fields returned by the
+ * Projection for a {@link Request}. Only the fields returned by the
  * methods specified in this interface will be returned when this projection
  * is used.
  *
@@ -13,23 +13,53 @@ import org.springframework.data.rest.core.config.Projection;
 @Projection(types = {RequestImpl.class})
 public interface RequestProjection {
 
+  /**
+   * Gets the request identifier
+   * @return Request identifier
+   */
   String getRequestId();
 
+  /**
+   * Gets the request status (IN_PROGRESS, FOR_CONFIGURATION, etc)
+   * @return Request status
+   */
   String getStatus();
 
+  /**
+   * Gets the request type (CREATE, UPDATE, DELETE).
+   * @return The request type
+   */
   RequestType getType();
 
+  /**
+   * Gets the request creator
+   * @return Request creator
+   */
   String getCreator();
-
+  
+  /**
+   * Gets the request assignee 
+   * @return Request assignee
+   */
   String getAssignee();
 
+  /**
+   * Gets the request description
+   * @return Request description
+   */
   String getDescription();
 
+  /**
+   * Gets the request domain (TIM, CSAM, PSEN, ...)
+   * @return Request domain
+   */
   String getDomain();
 
+  /**
+   * Gets the request creation date
+   * @return Request creation date
+   */
   @Value("#{target.createdAt.millis}")
   Long getCreatedAt();
 
-//  @Value("#{target.lastModified.millis}")
-//  Long getLastModified();
 }
