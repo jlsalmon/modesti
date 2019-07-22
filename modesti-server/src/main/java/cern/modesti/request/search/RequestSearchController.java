6e87e8f195aa915e8c30a5183258d0694fc947c6
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2019-07-22 16:57:36 +0200

    [MOD-195] Search requests returns projections instead of raw data

--- modesti-server/src/main/java/cern/modesti/request/search/RequestSearchController.java
+++ modesti-server/src/main/java/cern/modesti/request/search/RequestSearchController.java
@@ -3,8 +3,9 @@ package cern.modesti.request.search;
 import cern.modesti.predicate.RsqlExpressionBuilder;
 import cern.modesti.request.Request;
 import cern.modesti.request.RequestImpl;
+import cern.modesti.request.RequestProjection;
 import cern.modesti.request.RequestRepository;
-import cern.modesti.request.hateoas.RequestResourceAssembler;
+import cern.modesti.request.hateoas.RequestProjectionResourceAssembler;
 import com.querydsl.core.types.Predicate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,20 +34,30 @@ public class RequestSearchController {
   private RequestRepository repository;
 
   @Autowired
-  private RequestResourceAssembler resourceAssembler;
+  private PagedResourcesAssembler<RequestProjection> assembler;
+  
+  @Autowired
+  private RequestProjectionResourceAssembler projectionAssembler;
 
+  /**
+   * Searches for requests in the repository using the provided query (if any)
+   * @param query Search query
+   * @param pageable Pagination information
+   * @return Page of requests fulfilling the search conditions
+   */
   @RequestMapping(value = "/api/requests/search", method = GET, produces = "application/json")
-  HttpEntity<PagedResources<Request>> search(@RequestParam("query") String query, Pageable pageable, PagedResourcesAssembler assembler) {
-    Page<? extends Request> requests;
+  ResponseEntity<PagedResources<?>> search(@RequestParam("query") String query, Pageable pageable) {
+    Page<RequestProjection> requests;
 
     if (!query.isEmpty()) {
       Predicate predicate = new RsqlExpressionBuilder<>(RequestImpl.class).createExpression(query);
       log.debug(predicate.toString());
-      requests = repository.findAll(predicate, pageable);
+      requests = repository.findAllProjectedBy(predicate, pageable);
     } else {
-      requests = repository.findAll(pageable);
+      requests = repository.findAllProjectedBy(pageable);
     }
 
-    return new ResponseEntity<>(assembler.toResource(requests, resourceAssembler), HttpStatus.OK);
+    PagedResources<Resource> resources = assembler.toResource(requests, projectionAssembler);
+    return ResponseEntity.ok(resources);
   }
 }
