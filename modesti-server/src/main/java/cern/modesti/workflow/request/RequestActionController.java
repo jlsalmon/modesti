472745634d4e6d4601231d195252e2a48b65fa1c
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2019-07-22 16:58:11 +0200

    Renaming class

--- modesti-server/src/main/java/cern/modesti/workflow/request/RequestActionController.java
+++ modesti-server/src/main/java/cern/modesti/workflow/request/RequestActionController.java
@@ -5,7 +5,6 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import cern.modesti.request.Request;
 import cern.modesti.request.RequestService;
 import cern.modesti.user.User;
@@ -20,6 +19,7 @@ import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
@@ -30,11 +30,11 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
  * @author Ivan Prieto Barreiro
  */
 @RepositoryRestController
-public class RequestController {
+public class RequestActionController {
 	
   @Autowired
   private RequestService requestService;
-
+  
   @RequestMapping(path="/requests/{id}", value="/requests/{id}", method=POST, produces="application/hal+json")
   @PreAuthorize("permitAll")
   public HttpEntity<Resource<Request>> action(@PathVariable("id") String requestId, @RequestBody RequestAction action,
@@ -44,10 +44,11 @@ public class RequestController {
     
     if (request != null) {
     	Resource<Request> resource = new Resource<>(request);
-    	resource.add(linkTo(methodOn(RequestController.class).action(requestId, action, principal)).withSelfRel());
+    	resource.add(linkTo(methodOn(RequestActionController.class).action(requestId, action, principal)).withSelfRel());
     	return new ResponseEntity<>(resource, HttpStatus.OK);
     }
     
     return new ResponseEntity<>(HttpStatus.OK);
   }
+  
 }
