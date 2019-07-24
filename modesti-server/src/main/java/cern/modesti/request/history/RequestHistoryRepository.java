915c12cf1312b44dfd05749f3aef88b947cacd6f
Ivan Prieto Barreiro <ivan.prieto.barreiro@cern.ch>
2019-07-24 12:15:17 +0200

    [MOD-330] Handle request history with multiple entries for the same requestId

--- modesti-server/src/main/java/cern/modesti/request/history/RequestHistoryRepository.java
+++ modesti-server/src/main/java/cern/modesti/request/history/RequestHistoryRepository.java
@@ -1,5 +1,7 @@
 package cern.modesti.request.history;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -9,5 +11,18 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(path = "requestHistories", collectionResourceRel = "requestHistories", itemResourceRel = "requestHistory")
 public interface RequestHistoryRepository extends MongoRepository<RequestHistoryImpl, String> {
 
-  RequestHistoryImpl findOneByRequestId(String requestId);
+  /**
+   * Find a single request by its request identifier
+   * @param requestId The request identifier
+   * @return The request with the specified identifier or null if it was not found
+   */
+//  RequestHistoryImpl findOneByRequestId(String requestId);
+  
+  /**
+   * Finds all the requests with the specified request identifier. This method was added
+   * after a bug was found, where the request history contains two documents for a requestId
+   * @param requestId The request identifier
+   * @return Request list with the specified identifier or null/empty list if it was not found
+   */
+  List<RequestHistoryImpl> findAllByRequestId(String requestId);
 }
